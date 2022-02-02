e775b58493b48488dd5e700e2bb5ea0beb7e7b78
Nikhil <poojarinikhil01@gmail.com>
2022-02-02 23:24:01 +0530

    no[e

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 D2={"NIKHIL":"SHAWARMA","KARTHIK":"BIRYANI", "MAHEK":"KABAB", "SNEHALI": "BAIGAN"}
 print(D2)
 print(D2["SNEHALI"])
+del D2["KARTHIK"]
+print(D2)
+d3=D2.copy()#for copy if you not use copy it will be pointer and reflect the changes you have done to D2
+del D2["MAHEK"]
+print("this is d3 : ")
+print(d3)
+print("this is D2 : ")
+print(D2)
