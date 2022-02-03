23639d529385f75317c591000157f71b2b884089
Nikhil <poojarinikhil01@gmail.com>
2022-02-03 21:36:05 +0530

    no[e

--- exercise_1.py
+++ exercise_1.py
@@ -1,5 +1,9 @@
 d4={"concatenate":"link (things) together in a chain or series.","arigato":"thank you"}
 val=input("Enter the word ")
-print("Meaning of ")
-print(val)
-print(d4[val])
+if val in d4:
+
+    print("Meaning of ")
+    print(val)
+    print(d4[val])
+else:
+    print("not in my dictionary")
--- firsr_if_els.py
+++ firsr_if_els.py
@@ -1,7 +1,7 @@
 var1=3
 var2=10
 myvar=int(input())
-if var1>myvar:
+if var1>=myvar:
     print("Too small")
 elif var1<myvar<var2:
     print("Average no")
