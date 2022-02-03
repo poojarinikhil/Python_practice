da12768d7552671fa60ba2e107dd54dc5aec2b2e
Nikhil <poojarinikhil01@gmail.com>
2022-02-03 21:19:11 +0530

    no[e

--- exercise_1.py
+++ exercise_1.py
@@ -1,8 +1,5 @@
 d4={"concatenate":"link (things) together in a chain or series.","arigato":"thank you"}
 val=input("Enter the word ")
-if val == d4.items():
-    print("Meaning of ")
-    print(val)
-    print(d4[val])
-else:
-    print("Not in my dictionary")
+print("Meaning of ")
+print(val)
+print(d4[val])
